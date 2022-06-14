1e99779dc9c1b5bb4384828122cd05afa506a6b1
alef0gli <alessandrolr22@gmail.com>
2022-06-14 17:37:34 +0200

    hooks.js and login.js are now updated

--- src/hooks.js
+++ src/hooks.js
@@ -8,20 +8,20 @@ import { connectToDatabase } from '$lib/db';
 // endpoint) accordingly. 
 // Sets context in endpoints
 // Try console logging context in your endpoints' HTTP methods to understand the structure
-export const handle = async({ request, resolve }) => {
+export const handle = async({ event, resolve }) => {
     // Connecting to DB
     // All database code can only run inside async functions as it uses await
     const dbConnection = await connectToDatabase();
     const db = dbConnection.db;
 
     // Getting cookies from request headers - all requests have cookies on them
-    const cookies = cookie.parse(request.headers.cookie || '');
-    request.locals.user = cookies;
+    const cookies = cookie.parse(event.request.headers.get('cookie') || '');
+    event.locals.user = cookies;
     console.log("handle - Cookies: ", cookies);
 
     // If there are no cookies, the user is not authenticated
     if (!cookies.session_id) {
-        request.locals.user.authenticated = false;
+        event.locals.user.authenticated = false;
     }
 
     // Searching DB for the user with the right cookie
@@ -31,22 +31,15 @@ export const handle = async({ request, resolve }) => {
 
     // If there is that user, authenticate him and pass his email to context
     if (userSession) {
-        request.locals.user.authenticated = true;
-        request.locals.user.email = userSession.email;
+        event.locals.user.authenticated = true;
+        event.locals.user.email = userSession.email;
     } else {
-        request.locals.user.authenticated = false;
+        event.locals.user.authenticated = false;
     }
 
-    const response = await resolve(request);
+    const response = await resolve(event);
 
-    return {
-        ...response,
-        headers: {
-            ...response.headers
-            // You can add custom headers here
-            // 'x-custom-header': 'potato'
-        }
-    };
+    return response
 };
 
 
@@ -55,12 +48,12 @@ export const handle = async({ request, resolve }) => {
 // SvelteKit server-renders a page.
 // Sets session on client-side
 // try console logging session in routes' load({ session }) functions
-export const getSession = async(request) => {
+export const getSession = async(event) => {
     // Pass cookie with authenticated & email properties to session
-    return request.locals.user ? {
+    return event.locals.user ? {
         user: {
             authenticated: true,
-            email: request.locals.user.email
+            email: event.locals.user.email
         }
     } : {};
 };
